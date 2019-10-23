63e1fdb7d9eff2d6327281594ad62cc0de94c993
Jawar19 <jawar19@student.sdu.dk>
2019-10-23 18:32:41 +0200

    Update CombatController.java

--- src/hero3/CombatController.java
+++ src/hero3/CombatController.java
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hero3;
 
 /**
@@ -14,20 +8,3 @@ public class CombatController
 {
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hero3;
-
-/**
- *
- * @author Linea
- */
-public class CombatController
-{
-    
-}
->>>>>>> 0145103f7f32a60880ca9c2a4105960877fc67eb
