3516be06779b1efd21509e00b5456ae92908d61a
Jawar19 <jawar19@student.sdu.dk>
2019-10-23 16:55:53 +0200

    Merge branch 'master' of https://github.com/LineaHoffmann/Hero3

---
+++ src/hero3/CombatController.java
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hero3;
+
+/**
+ *
+ * @author Linea
+ */
+public class CombatController
+{
+    
+}
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hero3;
+
+/**
+ *
+ * @author Linea
+ */
+public class CombatController
+{
+    
+}
+>>>>>>> 0145103f7f32a60880ca9c2a4105960877fc67eb
